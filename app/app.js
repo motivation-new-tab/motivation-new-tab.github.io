1a86ab904977ea6cee6c711f303b7833eab57114
Dmytrii Lysak <siefan95@gmail.com>
2019-12-01 20:11:20 +0200

    update code

--- app/app.js
+++ app/app.js
@@ -1,85 +1,30 @@
-(function () {
+$(document).ready(function () {
+    let localStorageData, parsingData;
 
-    let $ = document.getElementById.bind(document);
-    // let $$ = document.querySelectorAll.bind(document);
+    localStorageData = localStorage.birthdayDate;
+    if (localStorageData) {
+        $('#age-template').show();
+        setInterval(function () {
+            parsingData = new Date(parseInt(localStorageData));
+            let now = new Date;
+            let duration = now - parsingData;
+            let years = duration / 31556900000; // 1 year in millisecond
 
-    let App = function ($el) {
-        this.$el = $el;
-        this.load();
+            let majorMinor = years.toFixed(9).toString().split('.');
+            $('#year').text(majorMinor[0]);
+            $('#milliseconds').text(majorMinor[1]);
+        }, 100);
+    } else {
+        $('#dob-template').show();
+    }
 
-        this.$el.addEventListener(
-            'submit', this.submit.bind(this)
-        );
-
-        if (this.dob) {
-            this.renderAgeLoop();
-        } else {
-            this.renderChoose();
-        }
-    };
-
-    App.fn = App.prototype;
-
-    App.fn.load = function () {
-        let value;
-
-        value = localStorage.dob;
-        if (value)
-            this.dob = new Date(parseInt(value));
-    };
-
-    App.fn.save = function () {
-        if (this.dob)
-            localStorage.dob = this.dob.getTime();
-    };
-
-    App.fn.submit = function (e) {
+    $('form').on('submit', function (e) {
         e.preventDefault();
 
-        let input = this.$$('input')[0];
-        if (!input.valueAsDate) return;
+        let birthdayDate = $(this).find('#date')[0].valueAsDate;
+        if (!birthdayDate) return 'incorrect date';
 
-        this.dob = input.valueAsDate;
-        this.save();
-        this.renderAgeLoop();
-    };
-
-    App.fn.renderChoose = function () {
-        this.html(this.view('dob')());
-    };
-
-    App.fn.renderAgeLoop = function () {
-        setInterval(this.renderAge.bind(this), 100);
-    };
-
-    App.fn.renderAge = function () {
-        let now = new Date;
-        let duration = now - this.dob;
-        let years = duration / 31556900000; // 1 year in millisecond
-
-        let majorMinor = years.toFixed(9).toString().split('.');
-
-        requestAnimationFrame(function () {
-            this.html(this.view('age')({
-                year: majorMinor[0],
-                milliseconds: majorMinor[1]
-            }));
-        }.bind(this));
-    };
-
-    App.fn.$$ = function (sel) {
-        return this.$el.querySelectorAll(sel);
-    };
-
-    App.fn.html = function (html) {
-        this.$el.innerHTML = html;
-    };
-
-    App.fn.view = function (name) {
-        let $el = $(name + '-template');
-        return Handlebars.compile($el.innerHTML);
-    };
-
-    window.app = new App($('app'))
-
-})();
+        if (birthdayDate)
+            localStorage.birthdayDate = birthdayDate.getTime();
+    });
+});
